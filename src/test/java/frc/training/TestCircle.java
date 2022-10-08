eb05d690a89fd74ace6d560c0bc86e281f233d0f
Andrew Heitkamp <phoenixnet.phoenix@gmail.com>
2022-10-08 16:42:01 -0400

    Revert "changing deivices don't mind this"
    This reverts commit 491b9d2bba2137379320ca6d28dbf97b0807ddc4.

--- src/test/java/frc/training/TestCircle.java
+++ src/test/java/frc/training/TestCircle.java
@@ -5,6 +5,9 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
 
 public class TestCircle {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Emergency testing only.");
+    } 
 
     @Test
     public void testInputAndReturnRadiusOfUnitCicle() throws Exception {
@@ -40,28 +43,4 @@ public class TestCircle {
         Circle c = new Circle(new Point(1.0, -1.5), 2.0);
         assertEquals("((1.0,-1.5), r=2.0, Circle)", c + "");
     }
-    /*
-    @Test
-    public void testMovingCircleCenterPoint() throws Exception {
-        Circle c = new Circle(new Point(0.0, 0.0), 1.0);
-        c.moveCenter(0.5, 0.5);
-        assertEquals("(0.5,0.5)", c.getCenter() + "");
-        c.moveCenter(-1, -1);
-        assertEquals("(-0.5,-0.5)", c.getCenter() + "");
-    }
-
-    @Test
-    public void testChangingCircleRadius() throws Exception {
-        Circle c = new Circle(new Point(0.0, 0.0), 1.0);
-        c.changeRadius(0.5);
-        assertEquals("((0.0,0.0), r=1.5, Circle)", c + "");
-        c.changeRadius(-1);
-        assertEquals("((0.0,0.0), r=0.5, Circle)", c + "");
-    }
-
-    @Test
-    public void testChangingCircleRadiusErrorReachedZero() throws Exception {
-        Circle c = new Circle(new Point(0.0, 0.0), 1.0);
-        assertThrows(Exception.class, () -> c.changeRadius(-1.0));
-    }*/
 }
