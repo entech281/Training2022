262f714ec9abf9be0a71942a323873bb34a0dde4
Andrew Heitkamp <phoenixnet.phoenix@gmail.com>
2022-10-08 16:42:24 -0400

    Revert "Revert "changing deivices don't mind this""
    This reverts commit eb05d690a89fd74ace6d560c0bc86e281f233d0f.

--- src/main/java/frc/training/Circle.java
+++ src/main/java/frc/training/Circle.java
@@ -4,7 +4,6 @@ public class Circle {
 
     protected Point centerPoint;
     protected double Radius;
-    protected double Area;
 
     public Circle(Point centerPoint, double Radius) throws Exception {
         if (Radius <= 0.0) {
@@ -21,7 +20,18 @@ public class Circle {
     public double getArea() {
         return Math.PI*(this.Radius*this.Radius);
     }
+    /*
+    public void moveCenter(double x, double y) {
+        this.centerPoint.move(x, y);
+    }
 
+    public void changeRadius(double z) throws Exception {
+        if (Radius + z <= 0.0) {
+            throw new Exception("Radius must remain greater than 0."); 
+        }
+        this.Radius = this.Radius + z;
+    }
+    */
     public double getCircumference() {
         return Math.PI*this.Radius*2;
     }
