e596499204029066528d785321107ff86640ed39
Andrew Heitkamp <phoenixnet.phoenix@gmail.com>
2022-10-18 01:43:32 -0400

    somehow for got this check prev commit for changes

--- src/main/java/frc/training/Shape.java
+++ src/main/java/frc/training/Shape.java
@@ -4,4 +4,6 @@ public abstract class Shape {
     public abstract Point getCenter();
     public abstract double getArea();
     public abstract double getPerimeter();
+    @Override
+    public abstract String toString();
 }
